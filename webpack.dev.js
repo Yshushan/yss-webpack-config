c90d9c19861c707b135019f9299f581abd951054
yangshushan <yss_2016@outlook.com>
2018-01-08 10:12:43 +0800

    add .eslintignore

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,14 @@
-const webpackMerge=require('webpack-merge');
-const commonConfig=require('./webpack.common.js');
-const path=require('path');
+const webpackMerge = require('webpack-merge')
+const commonConfig = require('./webpack.common.js')
+const path = require('path')
 
-module.exports=webpackMerge(commonConfig,{
+module.exports = webpackMerge(commonConfig, {
   devtool: 'inline-source-map',
   devServer: {
     // contentBase: '/',
-    publicPath: '/', //打包后的bundle在内存中的输出位置, default: '/'
-    historyApiFallback: true, 
+    publicPath: '/', // 打包后的bundle在内存中的输出位置, default: '/'
+    historyApiFallback: true,
     noInfo: true,
     compress: true,
-  }
+  },
 })
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,21 +1,21 @@
-const webpackMerge=require('webpack-merge');
-const commonConfig=require('./webpack.common.js');
-const webpack=require('webpack');
-const UglifyJSPlugin=require('uglifyjs-webpack-plugin');
-const HtmlWebpackPlugin=require('html-webpack-plugin');
+const webpackMerge = require('webpack-merge')
+const commonConfig = require('./webpack.common.js')
+const webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports=webpackMerge(commonConfig,{
+module.exports = webpackMerge(commonConfig, {
   devtool: 'cheap-module-source-map',
   plugins: [
     new UglifyJSPlugin({
-       sourceMap: true
+      sourceMap: true,
     }),
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("production")
+      'process.env.NODE_ENV': JSON.stringify('production'),
     }),
     new HtmlWebpackPlugin({
       title: 'Chat App',
       // filename: 'assets/index.html'
-    })
-  ]
+    }),
+  ],
 })
